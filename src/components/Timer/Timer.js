4aff402b308bd0724810e364592b58a911efb88b
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-02-18 07:12:25 -0500

    change style to play and reset button

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -4,7 +4,7 @@ import CustomDisplay from "../CustomParts/CustomDisplay/CustomDisplay";
 const Timer = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column",width: 300, textAlign:"center" }}>
-      <CustomDisplay value={25} />
+      <CustomDisplay value={"25:00"} />
       <h3 style={{ color: "white", fontSize: 24 }}>Session:Break</h3>
     </div>
   );
