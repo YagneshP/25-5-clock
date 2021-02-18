876516cf57fafffb7c536b5384b6de7937e9ff14
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-02-17 21:42:02 -0500

    add reset button

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -2,6 +2,7 @@ import React from "react";
 import Break from "../Break/Break";
 
 import CustomPlayButton from "../CustomParts/CustomPlayButton/CustomPlayButton";
+import CustomResetButton from "../CustomParts/CustomResetButton/CustomResetButton";
 import Session from "../Session/Session";
 import Timer from "../Timer/Timer";
 
@@ -10,6 +11,7 @@ const Clock = () => {
   return (
     <div className="Clock">
       <CustomPlayButton />
+			<CustomResetButton/>
       <div style={{ display: "flex" }}>
         <Break value={5} />
         <Session value={25} />
---
+++ src/components/CustomParts/CustomResetButton/CustomResetButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./CustomResetButton.css"
+const CustomResetButton = () => {
+	return (
+		<div className="CustomResetButton">
+			
+		</div>
+	)
+}
+
+export default CustomResetButton
+
+
+
